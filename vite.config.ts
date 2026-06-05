8b856b5adfe23a1fedde174c560bf956d0ef518d
Eugene-Stone <eugenestone.work@gmail.com>
2026-06-05 15:50:03 +0300

    Update - deploy settings fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/react-crypto', // Для корректной работы GitHub Pages, еще добавлена строчка  "postbuild": "cp dist/index.html dist/404.html", в package.json
+	base: '/react-crypto/', // Для корректной работы GitHub Pages, еще добавлена строчка  "postbuild": "cp dist/index.html dist/404.html", в package.json
 });
